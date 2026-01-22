09d4b0393d45590c6b537ab2d8f70d45c265794f
Proteus Qian <sudaku233@outlook.com>
2026-01-22 09:49:40 +0000

    chore: update nixpkgs input
    - Update nixpkgs revision to `e4bae1bd10c9c57b2cf517953ab70060a828ee6f` (nixos-unstable as of Jan 17, 2026).
    - Update `flake.lock`.
    
    Signed-off-by: Proteus Qian <sudaku233@outlook.com>

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   description = "My nix develops";
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/1412caf7bf9e660f2f962917c14b1ea1c3bc695e";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/e4bae1bd10c9c57b2cf517953ab70060a828ee6f";
   outputs = {self, ...}@inputs: let
     supported_systems = [
       "aarch64-darwin"
